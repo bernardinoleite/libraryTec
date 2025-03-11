6ff638f262de445727c2cb1a14898b0f283efce6
bernardinoleite <bernardinoleite2008@gmail.com>
2025-03-11 04:27:11 +0100

    Add user routes for listing, counting, and deleting users

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,11 +1,23 @@
 import { Router } from "express";
 import { createUserController } from "../modules/acoount/useCases/createUser";
 import { authenticateUserController } from "../modules/acoount/useCases/authenticateUser";
+import { listUsersController } from "../modules/acoount/useCases/listUsers";
+import { countUsersController } from "../modules/acoount/useCases/countUsers";
+import { deleteUserController } from "../modules/acoount/useCases/deleteUser";
 
 const userRouter = Router()
 
 userRouter.post("/", (request, response) => {
     createUserController.handle(request, response)
+})
+userRouter.delete("/:id", (request, response) => {
+    deleteUserController.handle(request, response)
+})
+userRouter.get("/findAll", (request, response) => {
+    listUsersController.handle(request, response)
+})
+userRouter.get("/countAll", (request, response) => {
+    countUsersController.handle(request, response)
 })
 userRouter.post("/auth", (request, response) => {
     authenticateUserController.handle(request, response)
